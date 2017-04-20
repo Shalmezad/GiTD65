04bf6f1b885cdd6017921b0f44818fe8cf29a213
Richard Wardin <richardw@hesonline.com>
2017-04-19 20:25:40 -0400

    Disabled splash screen

--- source/Main.hx
+++ source/Main.hx
@@ -9,6 +9,10 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		var updateFrameRate:Int = 30;
+		var drawFrameRate:Int = 30;
+		var skipSplash:Bool = true;
+		var fullScreen:Bool = false;
+		addChild(new FlxGame(0, 0, PlayState, 1, updateFrameRate, drawFrameRate, skipSplash, fullScreen));
 	}
 }
